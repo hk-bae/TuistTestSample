e652bb1a8aaceb9812b94783456ebf0c8941c3a1
hk-bae <hanykeol95@naver.com>
2024-11-06 21:28:21 +0900

    tuist setup

---
+++ Project.swift
@@ -0,0 +1,31 @@
+import ProjectDescription
+
+let project = Project(
+    name: "MyApp",
+    targets: [
+        .target(
+            name: "MyApp",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.MyApp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["MyApp/Sources/**"],
+            resources: ["MyApp/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "MyAppTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.MyAppTests",
+            infoPlist: .default,
+            sources: ["MyApp/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "MyApp")]
+        ),
+    ]
+)
